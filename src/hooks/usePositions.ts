61e88af625e00ce0cdbb9b798ede6c3f87c0ddde
jh <>
2024-01-10 22:30:27 -0600

    adds additional grass near river edges

--- src/hooks/usePositions.ts
+++ src/hooks/usePositions.ts
@@ -1,15 +1,22 @@
 import { useMemo } from "react";
 import { instancePositions } from "../data/instance-positions";
+import { riverMouthPositions } from "src/data/river-mouth";
 import { LCG } from "src/lib/utils";
 export const usePositions = () => {
   const shuffledPositions = useMemo(() => {
-    const positions = [...instancePositions];
+    const positions = [...instancePositions, ...riverMouthPositions];
     const lcg = new LCG(42); // seeded random to reproduce the overall layout
     /**https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle */
     for (let i = positions.length - 1; i > 0; i--) {
       const j = Math.floor(lcg.random() * (i + 1));
       [positions[i], positions[j]] = [positions[j], positions[i]];
     }
+    const river = [...riverMouthPositions];
+    for (let i = river.length - 1; i > 0; i--) {
+      const j = Math.floor(lcg.random() * (i + 1));
+      [river[i], river[j]] = [river[j], river[i]];
+    }
+
     return positions;
   }, []);
   return shuffledPositions;
