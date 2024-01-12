a289019532367be200407373dbcf846de56a0c0b
jh <>
2024-01-12 01:54:08 -0600

    adds a more realistic water animation

--- src/components/Water.tsx
+++ src/components/Water.tsx
@@ -1,11 +1,11 @@
 import { MeshTransmissionMaterial } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { Leva, useControls } from "leva";
-import { useLayoutEffect, useRef } from "react";
+import { useRef } from "react";
 import { Color, type Mesh } from "three";
 
 const size = 200;
-const waveAmplitude = 0.2;
+const waveAmplitude = 0.1;
 const waveFrequency = 0.05;
 
 export const Water = () => {
@@ -33,7 +33,6 @@ export const Water = () => {
       distortion: { value: 0.75, min: 0, max: 1, step: 0.01 },
       distortionScale: { value: 0.01, min: 0.01, max: 1, step: 0.01 },
       temporalDistortion: { value: 0.1, min: 0, max: 1, step: 0.01 },
-      // temporalDistortion: { value: 0.05, min: 0, max: 1, step: 0.01 },
       clearcoat: { value: 0.1, min: 0, max: 1 },
       attenuationDistance: { value: 0.13, min: 0, max: 10, step: 0.01 },
       attenuationColor: "#ffffff",
@@ -49,10 +48,18 @@ export const Water = () => {
     const geometry = mesh.geometry;
     for (let h = 0; h < size; h++) {
       for (let w = 0; w < size; w++) {
-        let verticesIndex = h * size + w;
-        const z =
-          waveAmplitude * Math.sin(clock.elapsedTime + (h + w) * waveFrequency);
+        let verticesIndex = (h * size + w) * 3 + 2; // +2 for the z-coordinate
 
+        // let verticesIndex = h * size + w;
+        const timeFactor = clock.elapsedTime + (h + w) * waveFrequency;
+        // Smoothly varying wave patterns
+        const hWave =
+          waveAmplitude * Math.sin(h * waveFrequency + clock.elapsedTime);
+        const wWave =
+          waveAmplitude * Math.cos(w * waveFrequency + clock.elapsedTime);
+
+        // Offset based on combined wave patterns
+        const z = hWave + wWave;
         geometry.attributes.position.setZ(verticesIndex, z);
       }
     }
@@ -62,9 +69,8 @@ export const Water = () => {
 
   return (
     <>
-      {/* <Leva hidden /> */}
+      <Leva hidden />
       <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]} position={[0, 10, 0]}>
-        {/* <boxGeometry args={[200, 20, 200]} /> */}
         <planeGeometry args={[200, 200, 50, 50]} />
 
         <MeshTransmissionMaterial
