c6ade532a9b287966985239171bda54db3484ff2
jh <>
2024-01-12 02:06:28 -0600

    adjust theme on loading screen

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -4,12 +4,14 @@ export const LoadingScreen = () => {
   const { progress, active } = useProgress();
   return (
     <div
-      className={`fixed inset-0 bg-gradient-to-b from-[#b8c6db] to-[#f5f7fa] z-10 p-16 flex items-center justify-center ${
+      className={`fixed inset-0 bg-gradient-to-b from-[#667e75] to-[#4B5563] z-10 p-16 flex items-center justify-center ${
         !active && "hidden"
       }`}
       style={{ opacity: 1 - progress / 100 }}
     >
-      <h1 className="text-xl font-bold">Loading... ({progress}%)</h1>
+      <h1 className="text-xl font-bold">
+        Loading... ({Math.floor(progress)}%)
+      </h1>
     </div>
   );
 };
